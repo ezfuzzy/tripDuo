83acbd8e827dae95065786075cd4b9ceba8b12b9
ezfuzzy <mentor787@naver.com>
2024-08-20 17:15:17 +0900

    add: jwt settings

--- back/tripDuo/src/main/java/com/example/tripDuo/config/SecurityConfig.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/config/SecurityConfig.java
@@ -1,53 +1,69 @@
 package com.example.tripDuo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.tripDuo.filter.JwtFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	@Bean // 메소드에서 return해주는 객체를 Bean으로 만들어줌
+
+	@Autowired
+	private JwtFilter jwtFilter;
+
+	@Bean
 	public SecurityFilterChain securityFilerChain(HttpSecurity httpSecurity) throws Exception {
-	    
-	    String[] whiteList = { 
-		  	"/", "/error", 
-	
-		  	"/user/loginform", "/user/login_fail", 
-		  	"/user/signup_form", "/user/signup"
-	
-	    };
-	
-		httpSecurity
-		.headers((header) -> {
-			header.frameOptions(option -> option.sameOrigin());   
+
+		String[] whiteList = { 
+			"/", "/home",
+
+		};
+
+		httpSecurity.headers((header) -> {
+			header.frameOptions(option -> option.sameOrigin());
 		})
 		.csrf(csrf -> csrf.disable())
 		.authorizeHttpRequests((config) -> {
-		  config
-		  	.requestMatchers(whiteList).permitAll()
-		  	.requestMatchers("/admin/**").hasRole("ADMIN")
-		.requestMatchers("/staff/**").hasAnyRole("ADMIN", "STAFF")
-		  	.anyRequest().authenticated();
+	    	config
+    		.requestMatchers(whiteList).permitAll() // whiteList
+    		.requestMatchers("/admin/**").hasRole("ADMIN")
+    		.requestMatchers("/staff/**").hasAnyRole("ADMIN", "STAFF")
+    		.anyRequest().authenticated();
 		})
 		.sessionManagement((config) -> {
-		  config.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		});
-		// 	.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-		
-	    return httpSecurity.build();
+			config.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		})
+		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return httpSecurity.build();
 	}
-	  
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	// authenticationManager
+
+	@Bean
+	AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
+			UserDetailsService userDetailService) throws Exception {
+
+		return http.getSharedObject(AuthenticationManagerBuilder.class)
+				.userDetailsService(userDetailService)
+				.passwordEncoder(bCryptPasswordEncoder)
+				.and()
+				.build();
+	}
 }
---
+++ back/tripDuo/src/main/java/com/example/tripDuo/filter/JwtFilter.java
@@ -0,0 +1,90 @@
+package com.example.tripDuo.filter;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.tripDuo.service.CustomUserDetailsService;
+import com.example.tripDuo.util.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+	
+	@Value("${jwt.name}")
+	private String jwtName;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Autowired
+	private CustomUserDetailsService service;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		// 1. extract token from cookie
+		Cookie[] cookies = request.getCookies();
+
+		String jwtToken = "";
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (jwtName.equals(cookie.getName())) {
+					jwtToken = cookie.getValue();
+					break;
+				}
+			}
+		}
+
+		if (jwtToken.equals("")) {
+			String authHeader = request.getHeader("Authorization");
+			if (authHeader != null) {
+				jwtToken = authHeader;
+			}
+		}
+
+		// 2. get userName from token
+		String userName = null;
+		if (jwtToken.startsWith("Bearer+")) {
+			jwtToken = jwtToken.substring(7);
+			userName = jwtUtil.extractUsername(jwtToken);
+		}
+
+		// 3. get UserDetails object from DB
+		if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = service.loadUserByUsername(userName);
+
+			// 4. validate token
+			boolean isValid = jwtUtil.validateToken(jwtToken, userDetails);
+
+			// 5. if validate: login(spring security를 통과할)
+			if (isValid) {
+				// 사용자가 제출한 username, pwd와 같은 인증 자격 증명 저장
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+						null, userDetails.getAuthorities());
+
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				// Security context update - 1회성 로그인
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		}
+		System.out.println("[tripDuo log] jwt filter on :: " + new Date());
+
+		// 다음 spring 필터 체인 진행하기
+		filterChain.doFilter(request, response);
+	}
+}
