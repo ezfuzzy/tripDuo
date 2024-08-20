d0d8e70c204872076813ca7c60dbfa1997c02b61
ezfuzzy <mentor787@naver.com>
2024-08-20 18:14:52 +0900

    fix: issue 고침 - @ResponseBody

--- back/tripDuo/src/main/java/com/example/tripDuo/controller/HomeController.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/controller/HomeController.java
@@ -2,12 +2,26 @@ package com.example.tripDuo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+	@ResponseBody
+	@GetMapping("/")
+	public String index() {
+		return "welcome index";
+	}
+	
+	@ResponseBody
 	@GetMapping("/home")
 	public String home() {
 		return "welcome home";
+	}
+	
+	@ResponseBody
+	@GetMapping("/main")
+	public String main() {
+		return "welcome main";
 	}
 }
