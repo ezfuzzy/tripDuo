4756ea82d36ebe3cb21648f281332b389a361471
ezfuzzy <mentor787@naver.com>
2024-08-21 16:15:32 +0900

    add: user signup, user login 완료

--- back/tripDuo/src/main/java/com/example/tripDuo/service/UserServiceImpl.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.tripDuo.dto.UserDto;
+import com.example.tripDuo.entity.User;
 import com.example.tripDuo.repository.UserRepository;
 
 @Service
@@ -14,14 +15,12 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public UserDto getUserById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return UserDto.toDto(repo.findById(id).get());
 	}
 
 	@Override
 	public UserDto getUserByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return UserDto.toDto(repo.findByUsername(username));
 	}
 
 	@Override
