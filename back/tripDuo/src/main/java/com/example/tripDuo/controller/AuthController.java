64967c3d66296775330aa8d66002b1b106b758d6
ezfuzzy <mentor787@naver.com>
2024-08-22 17:50:51 +0900

    add: 본인인증 test

--- back/tripDuo/src/main/java/com/example/tripDuo/controller/AuthController.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.example.tripDuo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tripDuo.dto.UserDto;
@@ -25,4 +27,22 @@ public class AuthController {
 	public String signup(@RequestBody UserDto dto) {
 		return service.signup(dto);
 	}
+	
+    @PostMapping("/send")
+    public ResponseEntity<String> sendVerificationCode(@RequestParam String phoneNumber) {
+    	System.out.println(phoneNumber);
+        //service.sendVerificationCode(phoneNumber);
+        return ResponseEntity.ok("Verification code sent");
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyPhoneNumber(@RequestParam String phoneNumber,
+                                                    @RequestParam String verificationCode) {
+        boolean isVerified = service.verifyPhoneNumber(phoneNumber, verificationCode);
+        if (isVerified) {
+            return ResponseEntity.ok("Phone number verified successfully");
+        } else {
+            return ResponseEntity.badRequest().body("Invalid verification code");
+        }
+    }
 }
--- back/tripDuo/src/main/java/com/example/tripDuo/controller/HomeController.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.example.tripDuo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,5 +25,39 @@ public class HomeController {
 	@GetMapping("/main")
 	public String main() {
 		return "welcome main";
+	}
+	
+	@GetMapping("/tripDuo")
+	public String passTest() {
+		return "";
+	}
+	
+	String phone = "";
+	String v_code = "";
+	
+	@ResponseBody
+	@PostMapping("/api/v1/auth/sms")
+	public String sms(@RequestBody String phoneNum) {
+		System.out.println(phoneNum);
+		phone = phoneNum.substring(0, phoneNum.length()-1);
+		v_code = "1313";
+		return phone + " is right? code : " + v_code;
+	}
+	
+	@ResponseBody
+	@PostMapping("/api/v1/auth/sms/verify")
+	public String smsVerify(@RequestBody String str) {
+		String phoneNum = str.substring(0, 11);
+		String code = str.substring(11, str.length()-1);
+		
+		System.out.println("phoneNUm : " + phoneNum);
+		System.out.println("code : " + code);
+		
+		System.out.println(phone + " ::: " + v_code);
+		
+		if(phone.equals(phoneNum) && v_code.equals(code)) {
+			return "true";
+		}
+		return "false";
 	}
 }
