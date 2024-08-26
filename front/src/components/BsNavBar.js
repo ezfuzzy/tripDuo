a548e6df683760bac6212d6cbf97dbdd6cb753d0
jay <jjha0125@gmail.com>
2024-08-26 15:56:31 +0900

    "/" => "/home" home.js 경로 변경

--- front/src/components/BsNavBar.js
+++ front/src/components/BsNavBar.js
@@ -91,7 +91,7 @@ function BsNavBar() {
                         {toggleBtn}
                     </button>
 
-                    <NavbarBrand href="/" className='appName'>
+                    <NavbarBrand href="/home" className='appName'>
                         {/* <img alt="IconImage" src="img/logo.png" /> */}
                         TripDuo
                     </NavbarBrand>
@@ -121,7 +121,7 @@ function BsNavBar() {
             </Navbar>
             <Nav fill variant="tabs" defaultActiveKey="/">
                 <Nav.Item>
-                    <Nav.Link as={NavLink} to="/">국내 여행</Nav.Link>
+                    <Nav.Link as={NavLink} to="/home">국내 여행</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link as={NavLink} to="/home-inter">해외 여행</Nav.Link>
