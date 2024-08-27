141b640f7a5b927303d907a6472e9658018e78cd
jay <jjha0125@gmail.com>
2024-08-27 16:03:12 +0900

    sample user info  추가

--- back/tripDuo/src/main/java/com/example/tripDuo/TripDuoApplication.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/TripDuoApplication.java
@@ -34,11 +34,23 @@ public class TripDuoApplication {
 		User u4 = User.builder().username("cccc").password(encoder.encode("cccc")).gender("여자").role("user").build();
 		User u5 = User.builder().username("dddd").password(encoder.encode("dddd")).gender("남자").role("user").build();
 		
+		User t1 = User.builder().id(1).username("user1").password(encoder.encode("password1")).nickname("닉네임1").age(25).name("홍길동").gender("남자").phone_num("010-1234-5678").email("user1@example.com").profile_pics("profile1.jpg").profile_msg("안녕하세요!").cur_location("서울").verification_status("인증됨").account_status("정상").social_links("http://social1.com").role("user").ratings(4.5f).last_login("5분 전").build();
+		User t2 = User.builder().id(2).username("user2").password(encoder.encode("password2")).nickname("닉네임2").age(30).name("김철수").gender("남자").phone_num("010-2345-6789").email("user2@example.com").profile_pics("profile2.jpg").profile_msg("안녕하세요!").cur_location("부산").verification_status("인증됨").account_status("정상").social_links("http://social2.com").role("user").ratings(3.8f).last_login("10분 전").build();
+		User t3 = User.builder().id(3).username("user3").password(encoder.encode("password3")).nickname("닉네임3").age(22).name("이영희").gender("여자").phone_num("010-3456-7890").email("user3@example.com").profile_pics("profile3.jpg").profile_msg("안녕하세요!").cur_location("대구").verification_status("인증됨").account_status("정상").social_links("http://social3.com").role("user").ratings(4.2f).last_login("15분 전").build();
+		User t4 = User.builder().id(4).username("user4").password(encoder.encode("password4")).nickname("닉네임4").age(28).name("박민수").gender("남자").phone_num("010-4567-8901").email("user4@example.com").profile_pics("profile4.jpg").profile_msg("안녕하세요!").cur_location("인천").verification_status("인증됨").account_status("정상").social_links("http://social4.com").role("user").ratings(4.0f).last_login("20분 전").build();
+		User t5 = User.builder().id(5).username("user5").password(encoder.encode("password5")).nickname("닉네임5").age(35).name("최지훈").gender("남자").phone_num("010-5678-9012").email("user5@example.com").profile_pics("profile5.jpg").profile_msg("안녕하세요!").cur_location("광주").verification_status("인증됨").account_status("정상").social_links("http://social5.com").role("user").ratings(4.1f).last_login("20분 전").build();
+		
 		userRepo.save(u1);
 		userRepo.save(u2);
 		userRepo.save(u3);
 		userRepo.save(u4);
 		userRepo.save(u5);
+		
+		userRepo.save(t1);
+		userRepo.save(t2);
+		userRepo.save(t3);
+		userRepo.save(t4);
+		userRepo.save(t5);
 	}
 
 }
