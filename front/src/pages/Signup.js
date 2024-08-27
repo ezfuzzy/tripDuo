15098f83af9f97da33e9ad12bcbc98c9cd4cdadb
cloth <pbp0417@gmail.com>
2024-08-27 18:30:58 +0900

    feat : 유효성 검사, 휴대폰 인증을 거치면 회원가입 버튼 활성화 기능 추가

--- front/src/pages/Signup.js
+++ front/src/pages/Signup.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
 
 function Signup() {
   const [username, setUsername] = useState('')
@@ -9,17 +10,22 @@ function Signup() {
   const [isValidNickname, setIsValidNickname] = useState(true)
 
   const [password, setPassword] = useState('')
-  const [hasLowerCase, setHasLowerCase] = useState(false);
-  const [hasUpperCase, setHasUpperCase] = useState(false);
-  const [hasNumber, setHasNumber] = useState(false);
-  const [hasSpecialChar, setHasSpecialChar] = useState(false);
-  const [isValidLength, setIsValidLength] = useState(false);
-  const [showValidationMessage, setShowValidationMessage] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [isPasswordMatched, setIsPasswordMatched] = useState(true)
+  const [hasLowerCase, setHasLowerCase] = useState(false)
+  const [hasUpperCase, setHasUpperCase] = useState(false)
+  const [hasNumber, setHasNumber] = useState(false)
+  const [hasSpecialChar, setHasSpecialChar] = useState(false)
+  const [isValidLength, setIsValidLength] = useState(false)
+  const [showValidationMessage, setShowValidationMessage] = useState(false)
 
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [verificationCode, setVerificationCode] = useState('');
-  const [isCodeSent, setIsCodeSent] = useState(false);
-  const [isVerified, setIsVerified] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [verificationCode, setVerificationCode] = useState('')
+  const [isCodeSent, setIsCodeSent] = useState(false)
+  const [isVerified, setIsVerified] = useState(false)
+
+  const [isAllChecked, setIsAllChecked] = useState(false)
+  const navigate = useNavigate()
 
   const validateUsername = (value) => {
     // 아이디 : 영어 소문자와 숫자로 이루어진 6~16자리
@@ -27,12 +33,13 @@ function Signup() {
     return regex.test(value)
   }
   const validatePassword = (value) => {
-    setHasLowerCase(/[a-z]/.test(value));
-    setHasUpperCase(/[A-Z]/.test(value));
-    setHasNumber(/\d/.test(value));
-    setHasSpecialChar(/[!@#$%^&*]/.test(value));
-    setIsValidLength(value.length >= 8 && value.length <= 15);
-  };
+    // 비밀번호 : 영어 대소문자, 숫자, 특수문자를 포함한 8~15자리
+    setHasLowerCase(/[a-z]/.test(value))
+    setHasUpperCase(/[A-Z]/.test(value))
+    setHasNumber(/\d/.test(value))
+    setHasSpecialChar(/[!@#$%^&*]/.test(value))
+    setIsValidLength(value.length >= 8 && value.length <= 15)
+  }
   const validateNickname = (value) => {
     // 닉네임 : 한글, 영어 대소문자, 숫자로 이루어진 2~16자리
     const regex = /^[가-힣a-zA-Z0-9]{2,16}$/
@@ -50,6 +57,9 @@ function Signup() {
     validatePassword(value)
     setShowValidationMessage(true)
   }
+  const confirmPasswordHandleChange = (e) => {
+    setConfirmPassword(e.target.value);
+  }
   const nicknameHandleChange = (e) => {
     const value = e.target.value
     setNickName(value)
@@ -57,42 +67,53 @@ function Signup() {
   }
 
   const phoneNumberHandleChange = (e) => {
-    setPhoneNumber(e.target.value);
-  };
+    setPhoneNumber(e.target.value)
+  }
   const verificationCodeHandleChange = (e) => {
-    setVerificationCode(e.target.value);
-  };
+    setVerificationCode(e.target.value)
+  }
   const sendVerificationCode = () => {
     axios.post('/api/v1/auth/send', phoneNumber)
-      .then((response) => {
-        setIsCodeSent(true);
-        alert('인증 코드가 전송되었습니다.');
-        // 인증 코드 입력란 + 인증 확인 버튼 랜더링 코드        
+      .then((res) => {
+        setIsCodeSent(true)
+        alert('인증 코드가 전송되었습니다.')
       })
-      .catch((error) => {
-        alert('인증 코드 전송에 실패했습니다.');
-      });
-  };
+      .catch((err) => {
+        alert('인증 코드 전송에 실패했습니다.')
+      })
+  }
   const verifyPhoneNumber = () => {
-    const str = phoneNumber + verificationCode;
+    const str = phoneNumber + verificationCode
     axios.post('/api/v1/auth/verify', str)
-      .then((response) => {
-        setIsVerified(true);
-        alert('휴대폰 번호가 성공적으로 인증되었습니다.');
+      .then((res) => {
+        setIsVerified(true)
+        alert('휴대폰 번호가 성공적으로 인증되었습니다.')
       })
-      .catch((error) => {
-        alert('인증 코드가 잘못되었습니다.');
-      });
-  };
+      .catch((err) => {
+        alert('인증 코드가 잘못되었습니다.')
+      })
+  }
 
   useEffect(() => {
     if (hasLowerCase && hasUpperCase && hasNumber && hasSpecialChar && isValidLength) {
-      setIsValidPassword(true);
-      setShowValidationMessage(false);
+      setIsValidPassword(true)
+      setShowValidationMessage(false)
     } else {
-      setIsValidPassword(false);
+      setIsValidPassword(false)
     }
-  }, [hasLowerCase, hasUpperCase, hasNumber, hasSpecialChar, isValidLength]);
+  }, [hasLowerCase, hasUpperCase, hasNumber, hasSpecialChar, isValidLength])
+
+  useEffect(() => {
+    setIsPasswordMatched(password === confirmPassword);
+  }, [password, confirmPassword])
+
+  useEffect(() => {
+    if (isValidUsername && isValidPassword && isValidNickname && isVerified) {
+      setIsAllChecked(true)
+    } else {
+      setIsAllChecked(false)
+    }
+  }, [isValidUsername, isValidPassword, isValidNickname, isVerified])
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -101,7 +122,7 @@ function Signup() {
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-6 sm:gap-x-4">
-          <div className="sm:col-span-3">
+          <div className="sm:col-span-6">
             <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
               아이디
             </label>
@@ -123,7 +144,7 @@ function Signup() {
             </div>
           </div>
 
-          <div className="sm:col-span-3">
+          <div className="sm:col-span-6">
             <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
               비밀번호
             </label>
@@ -150,6 +171,31 @@ function Signup() {
               )}
             </div>
           </div>
+
+          <div className="sm:col-span-6">
+            <label htmlFor="confirmPassword" className="block text-sm font-semibold leading-6 text-gray-900">
+              비밀번호 확인
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                value={confirmPassword}
+                onChange={confirmPasswordHandleChange}
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {!isPasswordMatched && (
+                <div className="mt-2 text-sm">
+                  <p className="mt-2 text-sm text-red-600">
+                    비밀번호가 일치하지 않습니다.
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+
           <div className="sm:col-span-6">
             <label htmlFor="nickname" className="block text-sm font-semibold leading-6 text-gray-900">
               닉네임
@@ -241,14 +287,17 @@ function Signup() {
           <button
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            disabled={!isVerified}  // 인증 성공 후에만 버튼 활성화
+            disabled={!isAllChecked}  // 인증 성공 후에만 버튼 활성화
+            onClick={()=>{
+              navigate("/completedSignup")
+            }}
           >
             회원가입
           </button>
         </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default Signup;
+export default Signup
