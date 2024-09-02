8259686630f66fc541fb41b5801e013cd7dc1692
jay <jjha0125@gmail.com>
2024-09-02 17:13:27 +0900

    add: HomeMate 에 링크 추가, MateBoard 페이지 정보 전환 세팅

--- front/src/pages/HomeMate.js
+++ front/src/pages/HomeMate.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HomeMate(props) {
     return (
         <div>
             <h1>여행 메이트 페이지</h1>
+            <Link to={"/mateBoard"}>메이트 게시판</Link>
         </div>
     );
 }
