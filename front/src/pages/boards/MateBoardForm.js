ec9bf2a240e16fb09f39dbedff00b9d70a623840
jay <jjha0125@gmail.com>
2024-09-02 16:36:46 +0900

    add:메이트 게시판 기본 셋팅

---
+++ front/src/pages/boards/MateBoardForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MateBoardForm(props) {
+    return (
+        <div>
+            <h1>메이트 게시판 작성 폼</h1>
+        </div>
+    );
+}
+
+export default MateBoardForm;
