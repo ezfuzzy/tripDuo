94a804cf8f6dc60531b67638ec2dad4f41ad2426
jay <jjha0125@gmail.com>
2024-09-04 17:45:05 +0900

    add:MateBoard 추가중 0904

--- front/src/pages/boards/MateBoard.js
+++ front/src/pages/boards/MateBoard.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function MateBoard() {
+
   //배열 안에서 객체로 관리
   const [pageData, setPageData] = useState([
     {
@@ -10,8 +11,8 @@ function MateBoard() {
       userId: "Sample Id",
       type: "Sample Type",
       title: "SAMPLE TITLE",
-      locationCountry: "SampCountry",
-      locationCity: "SampCity",
+      Country: "",
+      City: "강릉",
       tags: "",
       views: "165",
       likes: "6",
@@ -25,8 +26,8 @@ function MateBoard() {
       userId: "Sample Id2",
       type: "Sample Type2",
       title: "SAMPLE TITLE2",
-      locationCountry: "SampCountry2",
-      locationCity: "SampCity2",
+      Country: "베트남",
+      City: "다낭",
       tags: "",
       views: "178",
       likes: "12",
@@ -38,7 +39,8 @@ function MateBoard() {
   ]);
 
   const [domesticInternational, setDomesticInternational] =
-    useState("International");
+    useState("Domestic");
+  const [pageTurn, setPageTurn] = useState("to International"); // 페이지 전환 버튼
   const [whereAreYou, setWhereAreYou] = useState(null);
 
   //이벤트 관리부
@@ -53,22 +55,25 @@ function MateBoard() {
   };
 
   useEffect(() => {
-    if (domesticInternational === "International") {
-      // 국내 상태에서 눌렀을 때
+    if (domesticInternational === "Domestic") {
+      // 국내 상태
       //국내 메이트 게시판 요청
       setWhereAreYou("국내 여행 메이트 페이지");
-    } else if (domesticInternational === "Domestic") {
+      setPageTurn("to International");
+    } else if (domesticInternational === "International") {
       //해외 메이트 게시판 요청
       setWhereAreYou("해외 여행 메이트 페이지");
+      setPageTurn("to Domestic");
     }
   }, [domesticInternational]);
 
   return (
     <>
-      <h3>{whereAreYou}</h3>
+      <h3>{whereAreYou}</h3><NavLink to={"/mateBoard/new"}>새글 작성</NavLink>
+      <button className="border border-1 bg-light-green-200" onClick={handleButtonClick}>{pageTurn}</button>
+
       {/* 메이트 게시판 테이블 */}
       <table className="table-auto w-full border divide-y divide-x divide-gray-200">
-
         <thead className="text-center">
           <tr>
             <th>번호</th>
@@ -79,7 +84,6 @@ function MateBoard() {
             <th>좋아요</th>
           </tr>
         </thead>
-
         <tbody className="text-center divide-y">
           {pageData.map((item) => (
             <tr key={item.id}>
@@ -87,28 +91,22 @@ function MateBoard() {
               <td className="text-left">
                 {domesticInternational === "Domestic" ? (
                   <>
-                    <span className="border border-white bg-sky-200 text-gray-800 rounded px-2">{`#${item.locationCountry}`}</span>
-                    <span className="border border-white bg-sky-200 text-gray-800 rounded px-1">{`#${item.locationCity}`}</span>
+                    <span className="border border-white bg-sky-200 text-gray-800 rounded px-1">{`#${item.Country}`}</span>
+                    <span className="border border-white bg-sky-200 text-gray-800 rounded px-1">{`#${item.City}`}</span>
                   </>
                 ) : (
-                  <span className="border border-white bg-sky-200 text-gray-800 rounded px-1">{`#${item.locationCity}`}</span>
+                  <span className="border border-white bg-sky-200 text-gray-800 rounded px-1">{`#${item.City}`}</span>
                 )}
                 <Link to={`/mateBoard/${item.id}/detail`}>{item.title}</Link>
-              </td>{/* 글 제목 */}
+              </td>
               <td>{item.userId}</td> {/* 작성자 */}
-              <td>{item.updatedAt ? item.updatedAt : item.createdAt}</td>{/* 작성일/ 수정일 */}
+              <td>{item.updatedAt ? item.updatedAt : item.createdAt}</td>
               <td>{item.views}</td> {/* 조회수 */}
               <td>{item.likes}</td>
             </tr>
           ))}
         </tbody>
-
       </table>
-
-      <button className="mt-30" onClick={handleButtonClick}>
-        to {domesticInternational}
-      </button>
-
     </>
   );
 }
--- front/src/pages/boards/MateBoardForm.js
+++ front/src/pages/boards/MateBoardForm.js
@@ -1,11 +1,109 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import E from "react-script";
 
 function MateBoardForm(props) {
-    return (
+  const [selectedBorder, setSelectedBorder] = useState("domestic");
+  const [selectedContinent, setSelectedContinent] = useState();
+  const [selectedCountry, setSelectedCountry] = useState(); // 초기값 설정
+  const [selectedRegion, setSelectedRegion] = useState();
+
+  const countries = {
+    asia: [
+      "중국",
+      "일본",
+      "대만",
+      "몽골",
+      "라오스",
+      "말레이시아",
+      "베트남",
+      "태국",
+      "필리핀",
+    ],
+    europe: [
+      "영국",
+      "프랑스",
+      "독일",
+      "스페인",
+      "포르투갈",
+      "스위스",
+      "벨기에",
+      "네덜란드"
+    ],
+  };
+
+
+  return (
+    <>
+        <Link to={"/mateBoard"}>Mate</Link>
+      <h3>메이트 게시판 작성 폼</h3>
+
+      <form action="">
         <div>
-            <h1>메이트 게시판 작성 폼</h1>
+          <select
+            value={selectedBorder}
+            onChange={(e) => {
+              setSelectedBorder(e.target.value);
+            }}
+            name="border"
+            id="border"
+          >
+            <option value="domestic">국내</option>
+            <option value="international">해외</option>
+          </select>
+
+          {selectedBorder === "international" && (
+            <select
+              value={selectedContinent}
+              onChange={(e) => setSelectedContinent(e.target.value)}
+            >
+              <option value="">대륙</option>
+              <option value="asia">아시아</option>
+              <option value="europe">유럽</option>
+            </select>
+          )}
+
+          {selectedBorder === "international" && selectedContinent && (
+            <select
+              value={selectedCountry}
+              onChange={(e) => {
+                setSelectedCountry(e.target.value);
+              }}
+            >
+                <option value="">국가</option>
+              {countries[selectedContinent].map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          )}
         </div>
-    );
+
+        <div>
+            { selectedCountry ? <span className="border border-white bg-sky-200 text-gray-800 rounded px-1">#{selectedCountry}</span> : "" }
+            { selectedRegion ? <span className="border border-white bg-sky-200 text-gray-800 rounded px-1">#{selectedRegion}</span> : "" }
+        </div>
+        <div>
+          <label htmlFor="title">제목</label>
+          <input type="text" id="title" name="title"/>
+        </div>
+
+        <div>태그 입력</div>
+
+        <div>
+          <label htmlFor="content">내용</label>
+          <textarea
+            name="content"
+            id="content"
+            placeholder="메이트 구하는 내용 작성"
+          ></textarea>
+        </div>
+
+        <div></div>
+      </form>
+    </>
+  );
 }
 
 export default MateBoardForm;
