2b5249c64365e843c796c0521e31b4577c02d553
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-05 15:37:50 +0900

    add: post part 간단한 로직 완료

--- back/tripDuo/src/main/java/com/example/tripDuo/service/PostServiceImpl.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/service/PostServiceImpl.java
@@ -17,33 +17,37 @@ public class PostServiceImpl implements PostService{
 	
 	@Override
 	public List<PostDto> getPostList(String type) {
-		
-		// type 별로 처리 
+		// type별 get list 
 		return postRepo.findByType(type).stream().map(PostDto::toDto).toList();
 	}
 
 	@Override
 	public PostDto getPostById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return PostDto.toDto(postRepo.findById(id).get());
 	}
 
 	@Override
 	public void writePost(PostDto dto) {
 		postRepo.save(Post.toEntity(dto));
-		
 	}
 	
 	@Override
-	public Boolean updatePost(PostDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+	public PostDto updatePost(PostDto dto) {
+		
+		// put mapping이니까 정보 삭제 안되게 ... 
+		
+		// 1. 만약 기존의 모든 정보가 그대로 넘어오면
+		postRepo.save(Post.toEntity(dto));
+		
+		// 2. 만약 기존 정보중 일부만 넘어오면 -> controller에서 setId하고 넘겨준 다음에 로직 실행
+		// 협의하고 코드 짤 부분
+		
+		return dto; // 정보가 다 들어있는 dto 반환 - 안해줘도 되긴함
 	}
 
 	@Override
 	public void deletePost(Long id) {
-		// TODO Auto-generated method stub
-		
+		postRepo.deleteById(id);
 	}
 
 }
