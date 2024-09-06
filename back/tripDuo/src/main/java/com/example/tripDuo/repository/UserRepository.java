539c163cd425eccb793d7632dfc621fbd2bf2138
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-06 15:57:58 +0900

    add: add service method, refactor repo

--- back/tripDuo/src/main/java/com/example/tripDuo/repository/UserRepository.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package com.example.tripDuo.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.tripDuo.entity.User;
 
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	public User findByUsername(String username);
-	
-	public Optional<User> findById(Long id);
+	public User findByPhoneNumber(String phoneNumber);
+	public User findByEmail(String email);
 	
 	public void save(Long kakaoId);
 }
