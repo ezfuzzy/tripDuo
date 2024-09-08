545c4035d2852f305953d8cb71d9a20290e21849
jay <jjha0125@gmail.com>
2024-09-08 16:24:56 +0900

    add : mate 게시판 기본기능 설정 완료

--- front/src/pages/boards/MateBoardForm.js
+++ front/src/pages/boards/MateBoardForm.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { NavLink, useNavigate, useSearchParams } from "react-router-dom";
 
 // Require Editor CSS files.
 import 'froala-editor/css/froala_style.min.css';
@@ -12,21 +12,24 @@ import { Button } from "react-bootstrap";
 import { shallowEqual, useSelector } from "react-redux";
 
 function MateBoardForm(props) {
+
   const username = useSelector(state => state.username, shallowEqual);
+
+  const navigate = useNavigate()
 
   const [SearchParams, setSearchParams] = useSearchParams()
   const domesticInternational = SearchParams.get("di")
 
-  const [post, setPost] = useState({
-    tags:[],
-    viewCount : 10,
-    likeCount : 10,
-    rating : 80,
-    status : "recruiting"
-  }) // 게시물의 정보
+  const [post, setPost] = useState({}) // 게시물의 정보
+
+  //유저 정보 관리
+  const [userId, setUserId] = useState("")
+  const [nickname, setNickname] = useState("")
 
   //태그 관리
   const [tagInput, setTagInput] = useState("");
+  const [postTags, setPostTags] = useState([]);
+  const [tags, setTags] = useState();
 
   //테스트 데이터
   // const koreanRegion = ["제주도", "서울", "인천", "부산", "대전", "대구", "강원", "경기", "충북", "충남", "경북", "경남", "전북", "전남" ];
@@ -73,83 +76,74 @@ function MateBoardForm(props) {
 
     if (value.endsWith(" ") && value.trim() !== "") {
       const newTag = value.trim();
-      if (newTag !== "#" && newTag.startsWith("#") && !post.tags.includes(newTag)) {
-        setPost({
-          ...post,
-          tags : [
-            ...post.tags, newTag
-          ]
-        })
+      if (newTag !== "#" && newTag.startsWith("#") && !postTags.includes(newTag)) {
+        setPostTags([...postTags, newTag]);
         setTagInput("");
       }
     }
   };
 
-  const removeTag = (tagToRemove) => setPost({
-    ...post,
-    tags : [
-      post.tags.filter((tag) => tag !== tagToRemove)
-    ]
-  })
+  const removeTag = (tagToRemove) => setPostTags(postTags.filter((tag) => tag !== tagToRemove));
 
-  const handleSubmit = ()=>{
-    console.log(post);
-    
-    axios.post("/api/v1/posts/mate", post)
-    .then(res=>{
-      console.log(res.data)
-    })
-    .catch(error=>console.log(error))
+  const handleSubmit = async ()=>{
+    const updatedPost = {
+      ...post,
+      tags : tags,
+      userId : userId,
+      writer : nickname
+    };
+
+    try{
+      const response = await axios.post('/api/v1/posts/mate', updatedPost);
+
+      alert("새 글 작성에 성공하였습니다.")
+      navigate(`/mateBoard?di=${domesticInternational}`)
+    }catch(error){
+      console.log(error)
+    }
   }
 
+  useEffect(()=>{
+    username ?? navigate("/login")
+  },[username, navigate])
 
-  useEffect((post)=>{
+  useEffect(()=>{
+    setTags(postTags.join(", "))
+  },[postTags])
 
-    if(domesticInternational === "Domestic" && username){
-      if(username){
-        axios.get(`/api/v1/users/username/${username}`)
-        .then(res=>{
-            console.log(res.data.id);
-            setPost({
-              ...post,
-              country : "한국",
-              userId : res.data.id
-            })
+  useEffect((post) => {
+    
+    if (domesticInternational && username) {
+      axios.get(`/api/v1/users/username/${username}`)
+        .then(res => {
+          setPost({
+            ...post,
+            country: domesticInternational === "Domestic" ? "한국" : "",
+            tags:"",
+            viewCount: 10,
+            likeCount: 10,
+            rating: 80,
+            status: "recruiting",
+            city: "city"
+          });
+          setUserId(res.data.id)
+          setNickname(res.data.nickname)
+          
         })
         .catch(error => console.log(error));
-      }  
-    }else if(username){
-      if(username){
-        axios.get(`/api/v1/users/username/${username}`)
-        .then(res=>{
-            console.log(res.data.id);
-            setPost({
-              ...post,
-              userId : res.data.id
-            })
-        })
-        .catch(error => console.log(error));
-      } 
     }
-  },[domesticInternational,username])
-
-//   useEffect((post)=>{      
-//     if(username){
-//         axios.get(`/api/v1/users/username/${username}`)
-//         .then(res=>{
-//             console.log(res.data.id);
-//             setPost({
-//               ...post,
-//               userId : res.data.id
-//             })
-//         })
-//         .catch(error => console.log(error));
-//     }    
-// }, [username]);
+  }, [domesticInternational, username]);
 
   return (
     <>
-      <Link to={"/mateBoard"}>Mate</Link>
+      <NavLink
+        to={{
+          pathname: "/mateBoard",
+          search: `?di=${domesticInternational}`,
+        }}
+      >
+        Mate
+      </NavLink>
 
       <h3>{domesticInternational} 게시판 작성 폼</h3>
 
@@ -228,21 +222,20 @@ function MateBoardForm(props) {
             className="border p-2 w-1/3"
           />
           <div className="flex flex-wrap gap-2 mt-2">
-            {post.tags && post.tags.map((tag, index) => (
-                  <span
-                    key={index}
-                    className="bg-blue-100 text-blue-800 px-2 py-1 rounded-full flex items-center"
-                  >
-                    {tag}
-                    <button
-                      className="ml-2 p-0 h-4 w-4 text-black flex items-center justify-center"
-                      onClick={() => removeTag(tag)}
-                    >
-                      <span className="text-sm font-bold">&times;</span>
-                    </button>
-                  </span>
-                ))
-              }
+            {postTags.map((tag, index) => (
+              <span
+                key={index}
+                className="bg-blue-100 text-blue-800 px-2 py-1 rounded-full flex items-center"
+              >
+                {tag}
+                <button
+                  className="ml-2 p-0 h-4 w-4 text-black flex items-center justify-center"
+                  onClick={() => removeTag(tag)}
+                >
+                  <span className="text-sm font-bold">&times;</span>
+                </button>
+              </span>
+            ))}
           </div>
         </div>
 
