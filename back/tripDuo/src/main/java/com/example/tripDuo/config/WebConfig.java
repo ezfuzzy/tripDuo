85e268d39f544893d3caf12e44addbeff08e4832
chelsea <gh779900@gmail.com>
2024-09-09 18:12:35 +0900

    add: webconfig - cors 설정

---
+++ back/tripDuo/src/main/java/com/example/tripDuo/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.tripDuo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")  // 리액트가 실행 중인 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
