565229a5df6fd8be11f3bcc48f2a914eb142db26
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-11 19:37:45 +0900

    refactor: enum type 적용

--- back/tripDuo/src/main/java/com/example/tripDuo/enums/FollowType.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/enums/FollowType.java
@@ -2,5 +2,13 @@ package com.example.tripDuo.enums;
 
 public enum FollowType {
 	FOLLOW,   // 팔로우
-	BLOCK,    // 차단
+	BLOCK;    // 차단
+	
+    public static FollowType fromString(String type) {
+        try {
+            return FollowType.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid follow type: " + type, e);
+        }
+    }
 }
