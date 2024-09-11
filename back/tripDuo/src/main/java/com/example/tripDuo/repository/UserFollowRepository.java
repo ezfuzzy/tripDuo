b6437eab9ddd849c5fa2b858ac3d57a70e01915b
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-11 12:15:31 +0900

    add: user_profile_infos - 팔로우 정보 base

--- back/tripDuo/src/main/java/com/example/tripDuo/repository/UserFollowRepository.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/repository/UserFollowRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.tripDuo.entity.UserFollow;
 
 public interface UserFollowRepository extends JpaRepository<UserFollow, Long>{
-
+	long countByFollowerUserId(Long followerUserId); // profile view page에서 해당 유저가 follow 하는 사람이 몇명인지
+	long countByFolloweeUserId(Long followeeUserId); // profile view page에서 해당 유저를 follow 하는 사람이 몇명인지
 }
