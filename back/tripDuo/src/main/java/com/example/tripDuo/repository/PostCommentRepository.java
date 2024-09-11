f1d612ca063627eae514cdbdb5b4efa56187f043
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-11 12:16:38 +0900

    feat: post관련 부가기능 메소드 추가

--- back/tripDuo/src/main/java/com/example/tripDuo/repository/PostCommentRepository.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/repository/PostCommentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.tripDuo.entity.PostComment;
 
 public interface PostCommentRepository extends JpaRepository<PostComment, Long>{
-
+    long countByPostId(Long postId);
 }
--- back/tripDuo/src/main/java/com/example/tripDuo/repository/PostRepository.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/repository/PostRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.tripDuo.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-	public List<Post> findByTypeOrderByIdDesc(String type);
+	List<Post> findByTypeOrderByIdDesc(String type);
 }
