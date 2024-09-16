d182cb768c11a4dd3b6bf6f975deb8060cae3bd3
RaiN_S <cek516201@naver.com>
2024-09-16 14:22:11 +0900

    refactor: user follow 관련 메소드 수정

--- back/tripDuo/src/main/java/com/example/tripDuo/repository/UserFollowRepository.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/repository/UserFollowRepository.java
@@ -1,10 +1,17 @@
 package com.example.tripDuo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.tripDuo.entity.UserFollow;
+import com.example.tripDuo.enums.FollowType;
 
 public interface UserFollowRepository extends JpaRepository<UserFollow, Long>{
 	long countByFollowerUserId(Long followerUserId); // profile view page에서 해당 유저가 follow 하는 사람이 몇명인지
 	long countByFolloweeUserId(Long followeeUserId); // profile view page에서 해당 유저를 follow 하는 사람이 몇명인지
+	
+	List<Long> findFolloweeUserIdsByFollowerUserIdAndFollowType(Long followerUserId, FollowType followType);
+    List<Long> findFollowerUserIdsByFolloweeUserIdAndFollowType(Long followeeUserId, FollowType followType);
+    void deleteByFolloweeUserIdAndFollowTypeAndFollowerUserId(Long followeeUserId, FollowType followType, Long followerUserId);
 }
