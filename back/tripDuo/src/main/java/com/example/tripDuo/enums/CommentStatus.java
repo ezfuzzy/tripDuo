21e71992491603cd1b224cad67a1289273643803
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-19 15:13:26 +0900

    add: post와 postComment는 soft delete

--- back/tripDuo/src/main/java/com/example/tripDuo/enums/CommentStatus.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/enums/CommentStatus.java
@@ -1,7 +1,15 @@
 package com.example.tripDuo.enums;
 
 public enum CommentStatus {
-    VISIBLE,     // 공개
-    SECRET,      // 비밀댓글
-    DELETED      // 삭제됨
+    PUBLIC,     // 공개
+    PRIVATE,      // 비밀댓글
+    DELETED;      // 삭제됨
+    
+    public static CommentStatus fromString(String status) {
+        try {
+            return CommentStatus.valueOf(status.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid comment status: " + status, e);
+        }
+    }
 }
