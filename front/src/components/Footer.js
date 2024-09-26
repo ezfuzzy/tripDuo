0067d1cd3c198f51f0aff545b757ddffc707d9be
jay <jjha0125@gmail.com>
2024-09-26 17:18:25 +0900

    reafactor: Footer 재설정

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,17 +1,118 @@
-import React from 'react';
-import '../css/Footer.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Footer.css"
 
 function Footer(props) {
-    return (
-        <footer className='Footer'>
+  return (
+    <footer className="bg-white">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+
+            {/* LOGO */}
+          <div className="mb-6 md:mb-0">
+            <Link to={`/`} className="flex items-center">
+              <img src="/img/TripDuologo.png" className="w-40 h-auto me-3" alt="logo" />
+            </Link>
+          </div>
+
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            {/* RESOURCES */}
             <div>
-                <img className="logo" alt="IconImage" src="/img/img02.png" />
-                    <p>ROUTE SHARE</p>
-                    <p>회사정보 회사정보</p>
-                    <p>Copyright © 루트쉐어 All rights reserved</p>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase ">Resources</h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <a href="https://flowbite.com/" className="hover:underline">
+                    Flowbite
+                  </a>
+                </li>
+                <li>
+                  <a href="https://tailwindcss.com/" className="hover:underline">
+                    Tailwind CSS
+                  </a>
+                </li>
+              </ul>
             </div>
-        </footer>
-    );
+
+            {/* GitHub, Notion */}
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase ">Follow us</h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <Link to="https://github.com/ezfuzzy/tripDuo" className="hover:underline ">
+                    Github
+                  </Link>
+                </li>
+                <li>
+                  <Link to="https://www.notion.so/tripDuo-78610b624519486b94809610023286ea" className="hover:underline">
+                    Notion
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* LEGAL */}
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* copy rights */}
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center ">
+            © 2024{" "}
+            <a href="https://flowbite.com/" className="hover:underline">
+              TripDuo™
+            </a>
+            . All Rights Reserved.
+          </span>
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
+            <Link to="https://github.com/ezfuzzy/tripDuo" className="text-gray-500 hover:text-gray-900 ms-5">
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="sr-only">GitHub account</span>
+            </Link>
+            <Link
+              to="https://www.notion.so/tripDuo-78610b624519486b94809610023286ea"
+              className="text-gray-500 hover:text-gray-900 ms-5"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M4.459 4.208c.746.606 1.026.56 2.428.466l13.215-.793c.28 0 .047-.28-.046-.326L17.86 1.968c-.42-.326-.981-.7-2.055-.607L3.01 2.295c-.466.046-.56.28-.374.466zm.793 3.08v13.904c0 .747.373 1.027 1.214.98l14.523-.84c.841-.046.935-.56.935-1.167V6.354c0-.606-.233-.933-.748-.887l-15.177.887c-.56.047-.747.327-.747.933zm14.337.745c.093.42 0 .84-.42.888l-.7.14v10.264c-.608.327-1.168.514-1.635.514c-.748 0-.935-.234-1.495-.933l-4.577-7.186v6.952L12.21 19s0 .84-1.168.84l-3.222.186c-.093-.186 0-.653.327-.746l.84-.233V9.854L7.822 9.76c-.094-.42.14-1.026.793-1.073l3.456-.233l4.764 7.279v-6.44l-1.215-.139c-.093-.514.28-.887.747-.933zM1.936 1.035l13.31-.98c1.634-.14 2.055-.047 3.082.7l4.249 2.986c.7.513.934.653.934 1.213v16.378c0 1.026-.373 1.634-1.68 1.726l-15.458.934c-.98.047-1.448-.093-1.962-.747l-3.129-4.06c-.56-.747-.793-1.306-.793-1.96V2.667c0-.839.374-1.54 1.447-1.632"
+                />
+              </svg>
+              <span className="sr-only">Notion account</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
