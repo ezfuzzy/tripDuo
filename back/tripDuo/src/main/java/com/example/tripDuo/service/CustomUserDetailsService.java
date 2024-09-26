c1bfb138c6228336a0fe63af01351fdd2311903b
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-26 10:47:58 +0900

    refactor: 안쓰는 어노테이션 정리

--- back/tripDuo/src/main/java/com/example/tripDuo/service/CustomUserDetailsService.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/service/CustomUserDetailsService.java
@@ -3,7 +3,6 @@ package com.example.tripDuo.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
--- back/tripDuo/src/main/java/com/example/tripDuo/service/S3Service.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/service/S3Service.java
@@ -1,13 +1,10 @@
 package com.example.tripDuo.service;
 
-import java.io.File;
-import java.util.Optional;
-
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Service {
 	public String uploadFile(MultipartFile multipartFile);
 	public void deleteImage(String fileUrl);
 	public String createFileName(String fileName);
-	public String getFileExtension(String fileName);
+	public String getFileExtension(String fileName); 
 }
