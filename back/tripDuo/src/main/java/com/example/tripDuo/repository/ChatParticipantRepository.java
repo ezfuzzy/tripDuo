4a69081233d38abbdfdb9237ba2be73e8d61030a
hhan119 <gk9064@naver.com>
2024-10-04 14:58:01 +0900

    refactor : mate에서 1:1 채팅방 생성, 로그인시 웹 소켓 연결 수정

--- back/tripDuo/src/main/java/com/example/tripDuo/repository/ChatParticipantRepository.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/repository/ChatParticipantRepository.java
@@ -9,4 +9,6 @@ import com.example.tripDuo.entity.ChatParticipant;
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
 	// 사용자가 속한 채팅방 목록을 가져오는 메서드
     List<ChatParticipant> findByUserProfileInfoUserId(Long userId);
+
+	List<ChatParticipant> findByChatRoomId(Long roomId);
 }
