23804a5d88a510343541c8b5a8109d0f30362f2b
ezfuzzy <ezfuzzy062@gmail.com>
2024-10-07 17:48:08 +0900

    add: 소셜링크 로직 수정

--- front/src/pages/myPage/MyProfileForm.js
+++ front/src/pages/myPage/MyProfileForm.js
@@ -1,21 +1,21 @@
-import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router";
-import { Link } from "react-router-dom";
+import axios from "axios"
+import React, { useEffect, useRef, useState } from "react"
+import { shallowEqual, useDispatch, useSelector } from "react-redux"
+import { useNavigate, useParams } from "react-router"
+import { Link } from "react-router-dom"
 
 function MyProfileForm(props) {
-  const userId = useSelector((state) => state.userData.id, shallowEqual); // 접속된 사용자의 id
-  const username = useSelector((state) => state.userData.username, shallowEqual); // 접속된 사용자의 username
+  const userId = useSelector((state) => state.userData.id, shallowEqual) // 접속된 사용자의 id
+  const username = useSelector((state) => state.userData.username, shallowEqual) // 접속된 사용자의 username
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const profileImage = useRef();
-  const inputImage = useRef();
-  const inputNickname = useRef();
+  const profileImage = useRef()
+  const inputImage = useRef()
+  const inputNickname = useRef()
 
-  const navigate = useNavigate();
-  const [imageData, setImageData] = useState(null);
+  const navigate = useNavigate()
+  const [imageData, setImageData] = useState(null)
 
   const [profile, setProfile] = useState({
     id: "",
@@ -25,18 +25,19 @@ function MyProfileForm(props) {
     gender: "",
     profilePicture: "",
     profileMessage: "",
-    socialLinks: {
-      instagram: "",
-      github: "",
-    },
-  });
+    ratings: 1300,
+    socialLinks: "",
+  })
 
-  const { id } = useParams();
+  const tictokRef = useRef()
+  const instagramRef = useRef()
+
+  const { id } = useParams()
 
   // 닉네임 중복검사 관련
-  const [isDuplicate, setIsDuplicate] = useState(null);
-  const [isNicknameChanged, setIsNicknameChanged] = useState(false);
-  const [initialNickname, setInitialNickname] = useState();
+  const [isDuplicate, setIsDuplicate] = useState(null)
+  const [isNicknameChanged, setIsNicknameChanged] = useState(false)
+  const [initialNickname, setInitialNickname] = useState()
 
   // useEffect
   useEffect(() => {
@@ -44,60 +45,55 @@ function MyProfileForm(props) {
       .get(`/api/v1/users/${id}`)
       .then((res) => {
         if (!userId || userId !== res.data.userProfileInfo.userId) {
-          alert("잘못된 접근입니다.");
-          navigate(`/`);
+          alert("잘못된 접근입니다.")
+          navigate(`/`)
         }
+        console.log(res.data)
+        console.log(res.data.userProfileInfo)
+        setProfile(res.data.userProfileInfo)
 
-        const parsedSocialLinks = JSON.parse(res.data.userProfileInfo.socialLinks) || { instagram: "", github: "" }
-        console.log(parsedSocialLinks)
-        const userProfileInfo = {
-          ...res.data.userProfileInfo,
-          socialLinks: parsedSocialLinks
-        };
-        console.log(userProfileInfo)
+        const socialLinks = res.data.userProfileInfo.socialLinks || []
+        socialLinks.forEach((link) => {
+          const [platform, value] = link.split("+")
+          if (platform === "tictok") {
+            tictokRef.current.value = value.replace("tictok+", "")
+          } else if (platform === "instagram") {
+            instagramRef.current.value = value.replace("tictok+", "")
+          }
+        })
 
-        setProfile(userProfileInfo);
-
-        setInitialNickname(res.data.userProfileInfo.nickname); // 로딩된 닉네임 초기값 저장
+        setInitialNickname(res.data.userProfileInfo.nickname) // 로딩된 닉네임 초기값 저장
         if (res.data.userProfileInfo.profilePicture) {
-          setImageData(res.data.userProfileInfo.profilePicture);
+          setImageData(res.data.userProfileInfo.profilePicture)
         }
       })
-      .catch((error) => console.log(error));
-  }, [id]);
+      .catch((error) => console.log(error))
+  }, [id])
 
   //비밀번호 변경 페이지로 이동
   const toChangePassword = () => {
-    navigate(`/auth/${id}/changePassword`);
-  };
+    navigate(`/auth/${id}/changePassword`)
+  }
 
   // 이벤트 관리부
   const handleChange = (e) => {
+    const { name, value } = e.target
+
     setProfile({
       ...profile,
-      [e.target.name]: e.target.value,
-    });
+      [name]: value,
+    })
 
     if (e.target.name === "nickname") {
       if (e.target.value === initialNickname) {
         //처음 닉네임과 같은걸 입력했다면
-        setIsDuplicate(null); // 검사 결과 리셋
-        setIsNicknameChanged(false);
+        setIsDuplicate(null) // 검사 결과 리셋
+        setIsNicknameChanged(false)
       } else {
-        setIsNicknameChanged(true);
+        setIsNicknameChanged(true)
       }
     }
-  };
-
-  const handleSocialLinksChange = (e) => {
-    setProfile({
-      ...profile,
-      socialLinks: {
-        ...profile.socialLinks,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
+  }
 
   // *** 중복 검사 핸들러
   const handleCheckDuplicate = () => {
@@ -108,77 +104,76 @@ function MyProfileForm(props) {
         },
       })
       .then((res) => {
-        console.log(res.data);
-        setIsDuplicate(res.data);
+        console.log(res.data)
+        setIsDuplicate(res.data)
       })
-      .catch((error) => console.log(error));
-  };
+      .catch((error) => console.log(error))
+  }
 
   // *** 이미지 input ***
   const handleInputImage = () => {
-    const files = inputImage.current.files;
+    const files = inputImage.current.files
     if (files.length > 0) {
-      const file = files[0];
-      const reg = /image/;
+      const file = files[0]
+      const reg = /image/
 
       if (!reg.test(file.type)) {
-        alert("이미지 파일이 아닙니다");
-        return;
+        alert("이미지 파일이 아닙니다")
+        return
       }
 
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
       reader.onload = (e) => {
-        const data = e.target.result;
+        const data = e.target.result
         // file.dataUrl = data
 
-        setImageData(data);
-      };
+        setImageData(data)
+      }
     }
-  };
+  }
 
   // *** SUBMIT 이벤트 ***
   const handleSave = () => {
     // 닉네임이 수정되고 && 중복체크가 성공하지 않았다면
-    console.log(isNicknameChanged);
-    console.log(isDuplicate);
+    console.log(isNicknameChanged)
+    console.log(isDuplicate)
 
     if (isNicknameChanged && (isDuplicate === true || isDuplicate === null)) {
-      alert("사용 가능한 닉네임인지 확인해주세요");
+      alert("사용 가능한 닉네임인지 확인해주세요")
       //포커스 이동 !! 좀 더 위로 올라가게 수정하기
-      inputNickname.current.scrollIntoView({ behavior: "smooth" });
-      return;
+      inputNickname.current.scrollIntoView({ behavior: "smooth" })
+      return
     }
 
-    const formData = new FormData();
-    formData.append("id", profile.id);
-    formData.append("userId", profile.userId);
-    formData.append("age", profile.age || "");
-    formData.append("gender", profile.gender || "");
-    formData.append("nickname", profile.nickname);
+    console.log(tictokRef.current.value)
+    console.log(instagramRef)
+    const socialLinks = [`tictok+${tictokRef.current.value}`, `instagram+${instagramRef.current.value}`]
 
-    if (profile.socialLinks) {
-      const jsonString = JSON.stringify(profile.socialLinks);
-      formData.append("socialLinks", jsonString);
-    } else {
-      formData.append("socialLinks", {instagram:"", github:""})
-    }
+    const formData = new FormData()
+    formData.append("id", profile.id)
+    formData.append("userId", profile.userId)
+    formData.append("age", profile.age)
+    formData.append("gender", profile.gender)
+    formData.append("nickname", profile.nickname)
+    formData.append("socialLinks", socialLinks)
+    formData.append("profileMessage", profile.profileMessage || "")
+    formData.append("profilePicture", profile.profilePicture || "")
 
-    formData.append("profileMessage", profile.profileMessage ?? "");
-    formData.append("profilePicture", profile.profilePicture);
+    formData.append("curLocation", profile.curLocation || "")
+    formData.append("ratings", profile.ratings)
+    formData.append("lastLogin", profile.lastLogin || "")
 
-    formData.append("curLocation", profile.curLocation);
-    formData.append("ratings", profile.ratings);
-    formData.append("lastLogin", profile.lastLogin);
+    console.log(formData)
 
     //current.files[0] 의 값이 null 로 전달되어 에러가 발생
     //input type="file" 에 파일이 존재하면 multipart type의 데이터 append
     if (inputImage.current.files && inputImage.current.files.length > 0) {
-      formData.append("profileImgForUpload", inputImage.current.files[0]);
+      formData.append("profileImgForUpload", inputImage.current.files[0])
     }
 
     for (const [key, value] of formData.entries()) {
-      console.log(`${key}: ${value}`);
+      console.log(`${key}: ${value}`)
     }
 
     axios
@@ -193,52 +188,28 @@ function MyProfileForm(props) {
           username,
           nickname: res.data.nickname,
           profilePicture: res.data.profilePicture,
-        };
+        }
 
         // jwt token 갱신
-        localStorage.setItem("token", res.data.token);
-        dispatch({ type: "UPDATE_USER", payload: { userData } });
+        localStorage.setItem("token", res.data.token)
+        dispatch({ type: "UPDATE_USER", payload: { userData } })
 
-        alert("저장되었습니다.");
-        navigate(`/users/${id}/profile`);
+        alert("저장되었습니다.")
+        navigate(`/users/${id}/profile`)
       })
-      .catch((error) => console.log(error));
-  };
-
-  const instagramIcon = (
-    <svg
-      className="fill-current transition duration-700 ease-in-out text-gray-700 dark:text-gray-400 hover:text-pink-600 dark:hover:text-pink-600"
-      role="img"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg">
-      <title>Instagram</title>
-      <path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z" />
-    </svg>
-  );
-
-  const gitHubIcon = (
-    <svg
-      className="fill-current transition duration-700 ease-in-out text-gray-700 dark:text-gray-400 hover:text-black dark:hover:text-black"
-      role="img"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg">
-      <title>Github</title>
-      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-    </svg>
-  );
+      .catch((error) => console.log(error))
+  }
 
   // form 에서 전송되는 데이터 : profilePicture, profileMessage ,(email),(phoneNumber)
   return (
     <div className="container mx-auto p-4 max-w-[900px]">
-      <h3>Profile Update Form</h3>
       <div>
         <button type="button" className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600">
           <Link to={`/users/${id}/profile`}>돌아가기</Link>
         </button>
+      </div>
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold text-center text-gray-800">Profile Update Form</h1>
       </div>
       <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
         {/* 수정 폼 */}
@@ -292,7 +263,7 @@ function MyProfileForm(props) {
                 onChange={handleChange}
                 type="text"
                 name="nickname"
-                value={profile.nickname || ""}
+                value={profile.nickname}
                 className="flex-1 block w-full p-2 border border-gray-300 rounded-md"
               />
 
@@ -346,7 +317,7 @@ function MyProfileForm(props) {
               <input
                 type="text"
                 name="age"
-                value={profile.age || ""}
+                value={profile.age}
                 className="block w-full p-2 border border-gray-300 rounded-md"
                 onChange={handleChange}
               />
@@ -356,7 +327,7 @@ function MyProfileForm(props) {
                 gender
               </label>
               <select
-                value={profile.gender || ""}
+                value={profile.gender}
                 onChange={handleChange}
                 className="block w-full p-2 border border-gray-300 rounded-md"
                 name="gender"
@@ -371,26 +342,20 @@ function MyProfileForm(props) {
           {/* 소셜 링크 */}
           <div className="flex space-x-4">
             <div className="mb-3 flex-1">
-              <label htmlFor="instagram" className="block text-sm font-medium mb-1">
-                {instagramIcon}
-              </label>
+              <img src="/img/socialLinks/tictok.svg" className="w-6 mb-3" alt="tictok.svg" />
               <input
-                onChange={handleSocialLinksChange}
+                ref={tictokRef}
                 type="text"
-                name="instagram"
-                value={profile.socialLinks.instagram || ""}
+                name="socialLinks.tictok"
                 className="block w-full p-2 border border-gray-300 rounded-md"
               />
             </div>
             <div className="mb-3 flex-1">
-              <label htmlFor="github" className="block text-sm font-medium mb-1">
-                {gitHubIcon}
-              </label>
+              <img src="/img/socialLinks/instagram.svg" className="mb-3" alt="instagram.svg" />
               <input
-                onChange={handleSocialLinksChange}
+                ref={instagramRef}
                 type="text"
-                name="github"
-                value={profile.socialLinks.github || ""}
+                name="socialLinks.instagram"
                 className="block w-full p-2 border border-gray-300 rounded-md"
               />
             </div>
@@ -404,7 +369,7 @@ function MyProfileForm(props) {
             <textarea
               onChange={handleChange}
               name="profileMessage"
-              className="form-control w-full h-auto resize-none overflow-y-auto  border-gray-300 rounded-md"
+              className="form-control w-full h-auto resize-none overflow-y-auto"
               rows="5"
               value={profile.profileMessage || ""}
             />
@@ -421,9 +386,8 @@ function MyProfileForm(props) {
           </div>
         </form>
       </div>
-
     </div>
-  );
+  )
 }
 
-export default MyProfileForm;
+export default MyProfileForm
