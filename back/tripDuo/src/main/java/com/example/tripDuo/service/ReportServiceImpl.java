befd5c5ba1969baf727d46909f073772d33b8e44
RaiN_S <cek516201@naver.com>
2024-10-10 12:15:28 +0900

    add: getReportList 로직 개선

--- back/tripDuo/src/main/java/com/example/tripDuo/service/ReportServiceImpl.java
+++ back/tripDuo/src/main/java/com/example/tripDuo/service/ReportServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.tripDuo.service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,9 +12,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.tripDuo.dto.ChatRoomDto;
 import com.example.tripDuo.dto.ReportDto;
 import com.example.tripDuo.entity.ChatMessage;
+import com.example.tripDuo.entity.ChatParticipant;
 import com.example.tripDuo.entity.ChatRoom;
 import com.example.tripDuo.entity.Post;
 import com.example.tripDuo.entity.PostComment;
@@ -26,8 +27,10 @@ import com.example.tripDuo.entity.ReportToUser;
 import com.example.tripDuo.entity.ReportToUserReview;
 import com.example.tripDuo.entity.User;
 import com.example.tripDuo.entity.UserReview;
+import com.example.tripDuo.enums.AccountStatus;
 import com.example.tripDuo.enums.ReportTarget;
 import com.example.tripDuo.repository.ChatMessageRepository;
+import com.example.tripDuo.repository.ChatParticipantRepository;
 import com.example.tripDuo.repository.ChatRoomRepository;
 import com.example.tripDuo.repository.PostCommentRepository;
 import com.example.tripDuo.repository.PostRepository;
@@ -45,13 +48,15 @@ public class ReportServiceImpl implements ReportService {
     private final PostRepository postRepo;
     private final PostCommentRepository postCommentRepo;
     private final ChatRoomRepository chatRoomRepo;
+    private final ChatParticipantRepository chatParticipantRepo;
     private final ChatMessageRepository chatMessageRepo;
 
     final int REPORT_PAGE_SIZE = 10;
 
     public ReportServiceImpl(ReportRepository reportRepo, UserRepository userRepo,
-                             UserReviewRepository userReviewRepo, PostRepository postRepo, PostCommentRepository postCommentRepo,
-                             ChatRoomRepository chatRoomRepo, ChatMessageRepository chatMessageRepo) {
+                             UserReviewRepository userReviewRepo, PostRepository postRepo,
+                             PostCommentRepository postCommentRepo, ChatRoomRepository chatRoomRepo,
+                             ChatParticipantRepository chatParticipantRepo, ChatMessageRepository chatMessageRepo) {
 
         this.reportRepo = reportRepo;
         this.userRepo = userRepo;
@@ -59,6 +64,7 @@ public class ReportServiceImpl implements ReportService {
         this.postRepo = postRepo;
         this.postCommentRepo = postCommentRepo;
         this.chatRoomRepo = chatRoomRepo;
+        this.chatParticipantRepo = chatParticipantRepo;
         this.chatMessageRepo = chatMessageRepo;
     }
 
@@ -186,40 +192,56 @@ public class ReportServiceImpl implements ReportService {
         Page<Report> reports = reportRepo.findAll(ReportSpecification.searchReports(reportDto), pageable);
         List<Report> reportList = reports.getContent();
 
+        
+		List<ReportDto> reportDtoList = reportList.stream()
+                .map(ReportDto::toDto)
+                .collect(Collectors.toList());
+
+        // targetTypeList, targetIdList, targetAccountStatusList 가져오기
+        List<String> targetTypeList = reportDtoList.stream()
+                .map(ReportDto::getTargetType)
+                .collect(Collectors.toList());
+
+        List<Long> targetIdList = reportDtoList.stream()
+                .map(ReportDto::getTargetId)
+                .collect(Collectors.toList());
+
+        List<AccountStatus> targetAccountStatusList = reportList.stream()
+        .map(report -> {
+            if (report instanceof ReportToUser) {
+                return ((ReportToUser) report).getReportedUser().getAccountStatus();
+            } else if (report instanceof ReportToUserReview) {
+                return ((ReportToUserReview) report).getReportedUserReview().getReviewerUserProfileInfo().getUser().getAccountStatus();
+            } else if (report instanceof ReportToPost) {
+                return ((ReportToPost) report).getReportedPost().getUserProfileInfo().getUser().getAccountStatus();
+            } else if (report instanceof ReportToPostComment) {
+                return ((ReportToPostComment) report).getReportedPostComment().getUserProfileInfo().getUser().getAccountStatus();
+            } else if (report instanceof ReportToChatRoom) {
+                Long chatRoomId = ((ReportToChatRoom) report).getReportedChatRoom().getId();
+                ChatParticipant chatParticipant = chatParticipantRepo.findByChatRoomIdAndIsOwner(chatRoomId, true);
+                return chatParticipant.getUserProfileInfo().getUser().getAccountStatus();
+            } else if (report instanceof ReportToChatMessage) {
+                return ((ReportToChatMessage) report).getReportedChatMessage().getUserProfileInfo().getUser().getAccountStatus();
+            } else {
+                return null; // 다른 서브클래스의 경우 null 반환
+            }
+        })
+        .collect(Collectors.toList());
+
+
 		// 총 행 수, 페이지 수 계산
         long totalRowCount = reports.getTotalElements();
         int totalReportPages = (int) Math.ceil((double) totalRowCount / REPORT_PAGE_SIZE);
 
         return Map.of(
                 "list", reportList,
+                "targetTypeList", targetTypeList,
+                "targetIdList", targetIdList,
+                "targetAccountStatusList", targetAccountStatusList,
                 "pageNum", reportDto.getPageNum(),
                 "totalRowCount", totalRowCount,
                 "totalReportPages", totalReportPages
         );
-    }
-
-    public Object getReportDetail(Long reportId) {
-        Report existReport = reportRepo.findById(reportId)
-                    .orElseThrow(() -> new EntityNotFoundException("신고를 찾을 수 없습니다."));
-        ReportDto existReportDto = ReportDto.toDto(existReport);
-
-        // 신고 대상의 정보 가져오기
-        Object targetInfo = switch (ReportTarget.fromString(existReportDto.getTargetType())) {
-            case USER -> userRepo.findById(existReportDto.getTargetId())
-                    .orElseThrow(() -> new EntityNotFoundException("신고 대상 사용자를 찾을 수 없습니다."));
-            case USER_REVIEW -> userReviewRepo.findById(existReportDto.getTargetId())
-                    .orElseThrow(() -> new EntityNotFoundException("신고 대상 리뷰를 찾을 수 없습니다."));
-            case POST -> postRepo.findById(existReportDto.getTargetId())
-                    .orElseThrow(() -> new EntityNotFoundException("신고 대상 게시글을 찾을 수 없습니다."));
-            case POST_COMMENT -> postCommentRepo.findById(existReportDto.getTargetId())
-                    .orElseThrow(() -> new EntityNotFoundException("신고 대상 댓글을 찾을 수 없습니다."));
-            case CHAT_ROOM -> chatRoomRepo.findById(existReportDto.getTargetId())
-                    .orElseThrow(() -> new EntityNotFoundException("신고 대상 채팅방을 찾을 수 없습니다."));
-            case CHAT_MESSAGE -> chatMessageRepo.findById(existReportDto.getTargetId())
-                    .orElseThrow(() -> new EntityNotFoundException("신고 대상 채팅 메시지를 찾을 수 없습니다."));
-        };
-
-        return targetInfo;
     }
 
     /**
@@ -280,9 +302,8 @@ public class ReportServiceImpl implements ReportService {
             case CHAT_ROOM -> {
                 ChatRoom reportedChatRoom = chatRoomRepo.findById(existReportDto.getTargetId())
                         .orElseThrow(() -> new EntityNotFoundException("신고 대상 채팅방을 찾을 수 없습니다."));
-                Long chatRoomOwnerId = ChatRoomDto.toDto(reportedChatRoom).getOwnerId();
-                yield userRepo.findById(chatRoomOwnerId)
-                        .orElseThrow(() -> new EntityNotFoundException("신고 대상 사용자를 찾을 수 없습니다."));
+                ChatParticipant chatParticipant = chatParticipantRepo.findByChatRoomIdAndIsOwner(reportedChatRoom.getId(), true);
+                yield chatParticipant.getUserProfileInfo().getUser();
             }
             case CHAT_MESSAGE -> {
                 ChatMessage reportedChatMessage = chatMessageRepo.findById(existReportDto.getTargetId())
