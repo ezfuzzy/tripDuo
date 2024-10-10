75d2fea90c9c3a11d5852d7170703a051469fb6a
cloth <pbp0417@gmail.com>
2024-10-10 12:36:11 +0900

    chore: padding 값 조정

--- front/src/pages/Signup.js
+++ front/src/pages/Signup.js
@@ -303,7 +303,7 @@ function Signup() {
   }
 
   return (
-    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="isolate bg-white px-6 py-24 sm:py-24 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">회원가입</h2>
       </div>
