2e9d2eb5d16622aeebde0228e22d8e3899c75de8
cloth <pbp0417@gmail.com>
2024-10-10 17:43:36 +0900

    add: 커스텀 색 하나 더 추가

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      // 새로운 색상 추가
       colors: {
-        tripDuoGreen: '#006622',  // 새로운 색상 추가
+        tripDuoGreen: '#006622',  
+        tripDuoMint: '#66CC99', 
       },
     },
   },
