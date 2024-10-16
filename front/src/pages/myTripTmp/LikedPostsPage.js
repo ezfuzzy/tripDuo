5f11af50cf0bfa8b9e484d72bee061e479f732b7
jay <jjha0125@gmail.com>
2024-10-16 11:13:07 +0900

    add: 관심 메이트/ 관심 계획 게시판 뷰 페이지 통일

--- front/src/pages/myTripTmp/LikedPostsPage.js
+++ front/src/pages/myTripTmp/LikedPostsPage.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { shallowEqual, useSelector } from "react-redux"
+import { cityMapping, countryMapping } from "../../constants/mapping"
 
 function LikedPostsPage(props) {
   const loggedInUserId = useSelector((state) => state.userData.id, shallowEqual) // 로그인된 user의 id
@@ -18,26 +19,68 @@ function LikedPostsPage(props) {
       })
   }, [])
 
+  // city 또는 country 값에 따른 이미지 파일명 변환 함수
+  const getImageFileName = (city, country) => {
+    // city 값이 있으면 city에 맞는 이미지, 없으면 country에 맞는 이미지 반환
+    if (city && cityMapping[city]) {
+      return cityMapping[city]
+    } else if (country && countryMapping[country]) {
+      return countryMapping[country]
+    } else {
+      return "defaultImage" // 매핑되지 않은 경우 기본값 처리
+    }
+  }
+
   return (
     <>
-      <div>
+      <div className="container mx-auto p-4 max-w-[1024px]">
         {course ? (
           <>
-            <h3>좋아요한 게시글 목록</h3>
+            <div className="py-5">
+              <p className="font-bold text-xl text-center">관심 여행 계획</p>
+            </div>
             <ul className="space-y-4">
               {postList
                 .filter((post) => post.post.type === "COURSE")
-                .map((post) => (
-                  <li key={post.post.id} className="p-4 border rounded-lg shadow-md">
-                    <a href={`/posts/course/${post.post.id}/detail`} className="block">
-                      <h4 className="text-xl font-semibold">{post.post.title}</h4>
-                      <p className="text-gray-600">{post.post.description}</p>
-                      <p className="text-sm text-gray-500">
-                        작성일: {new Date(post.post.createdAt).toLocaleDateString()}
-                      </p>
-                    </a>
-                  </li>
-                ))}
+                .map((post) => {
+                  const imageFileName = getImageFileName(post.post.city, post.post.country)
+                  const imagePath = `/img/countryImages/${imageFileName}.jpg`
+                  return (
+                    <li
+                      key={post.post.id}
+                      className={`p-4 border rounded-lg shadow-md border border-green-600 hover:scale-102 transition duration-300 hover:shadow-xl`}
+                      style={{
+                        backgroundImage: `linear-gradient(to right,
+                    rgba(255, 255, 255, 1) 0%, 
+                    rgba(255, 255, 255, 1) 20%, 
+                    rgba(255, 255, 255, 0.5) 40%, 
+                    rgba(255, 255, 255, 0) 60%, 
+                    rgba(255, 255, 255, 0) 80%),
+                    url(${imagePath})`,
+                        backgroundSize: "cover", // 이미지 채우기
+                        backgroundPosition: "center",
+                        // /* 혼합 모드 설정 */
+                        mixBlendMode: "multiply",
+                      }}>
+                      <a href={`/posts/course/${post.post.id}/detail`} className="block">
+                        <div className="md:flex justify-between">
+                          <h4 className="text-xl font-semibold">{post.post.title}</h4>
+                          <p>
+                            <span className="bg-green-100 text-green-800 text-xs font-semibold px-2 py-1 rounded mr-2">
+                              #{post.post.city}
+                            </span>
+                            <span className="bg-green-100 text-green-800 text-xs font-semibold px-2 py-1 rounded">
+                              #{post.post.country}
+                            </span>
+                          </p>
+                        </div>
+                        <p className="text-sm text-gray-500">
+                          작성일: {new Date(post.post.createdAt).toLocaleDateString()}
+                        </p>
+                      </a>
+                    </li>
+                  )
+                })}
             </ul>
           </>
         ) : (
