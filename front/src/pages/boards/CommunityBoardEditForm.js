71975a45d6e9e3dfa021525a8c513ee07f98ce91
jay <jjha0125@gmail.com>
2024-10-17 15:52:41 +0900

    refactor: mate/community 수정 폼 수정

--- front/src/pages/boards/CommunityBoardEditForm.js
+++ front/src/pages/boards/CommunityBoardEditForm.js
@@ -1,76 +1,84 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import FroalaEditor from "react-froala-wysiwyg";
-import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import FroalaEditor from "react-froala-wysiwyg"
+import { Link, NavLink, useNavigate, useParams } from "react-router-dom"
 import LoadingAnimation from "../../components/LoadingAnimation"
 
 function CommunityBoardEditForm(props) {
   //로딩 상태 추가
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
 
-  const { id } = useParams();
+  const { id } = useParams()
 
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState({})
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
     // 로딩 애니메이션을 0.5초 동안만 표시
-    setLoading(true);
+    setLoading(true)
     setTimeout(() => {
-      setLoading(false);
-    }, 700);
+      setLoading(false)
+    }, 700)
 
     axios
       .get(`/api/v1/posts/${id}/update`)
       .then((res) => {
-        console.log(res.data);
+        console.log(res.data)
         // 데이터 전달 확인
-        setPost(res.data);
+        setPost(res.data)
       })
-      .catch((error) => console.log(error));
-  }, [id]);
+      .catch((error) => console.log(error))
+  }, [id])
 
   //수정 내용 put 요청
   const handleSubmit = () => {
     axios
       .put(`/api/v1/posts/${id}`, post)
       .then((res) => {
-        console.log(post);
+        console.log(post)
         //데이터 전달 확인
-        alert("수정에 성공하였습니다.");
-        navigate(`/posts/community/${id}/detail`); // 상세 페이지로 돌려보내기
+        alert("수정에 성공하였습니다.")
+        navigate(`/posts/community/${id}/detail`) // 상세 페이지로 돌려보내기
       })
-      .catch((error) => console.log(error));
-  };
+      .catch((error) => console.log(error))
+  }
 
   // handleChange 처럼 Post 값으로 관리한다.
   const handleModelChange = (e) => {
     setPost({
       ...post,
       content: e,
-    });
-  };
+    })
+  }
 
   const handleChange = (e) => {
     setPost({
       ...post,
       [e.target.name]: e.target.value,
-    });
-  };
+    })
+  }
 
   return (
     <div className="container mx-auto p-4 max-w-[900px]">
       {/* 로딩 애니메이션 */}
       {loading && <LoadingAnimation />}
       <div className="h-full bg-gray-100 p-6">
-        <NavLink
-          className="px-4 py-2 text-sm font-medium rounded-md bg-gray-600 text-gray-100"
-          to={`/posts/community/${id}/detail`}>
-          상세 페이지로
-        </NavLink>
-
-        <h3>{id} 번 게시물 수정 폼</h3>
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-3xl font-semibold text-gray-800">여행 코스 수정</h1>
+          <div className="flex space-x-2">
+            <button
+              onClick={() => navigate(`/posts/community/${id}/detail`)}
+              className="text-gray-700 bg-gray-200 hover:bg-gray-300 rounded-full text-sm px-5 py-2">
+              게시글로 돌아가기
+            </button>
+            <button
+              className="text-white bg-indigo-600 hover:bg-indigo-500 rounded-full text-sm px-5 py-2"
+              onClick={handleSubmit}>
+              수정 완료
+            </button>
+          </div>
+        </div>
 
         {/* 태그 */}
         <div className="flex flex-wrap gap-2 m-3">
@@ -84,7 +92,7 @@ function CommunityBoardEditForm(props) {
             ))}
         </div>
 
-        <form>
+        <div>
           <div className="m-3">
             <label className="font-semibold" htmlFor="title">
               제목
@@ -100,7 +108,7 @@ function CommunityBoardEditForm(props) {
           </div>
 
           <div>
-            <label className="font-semibold" htmlFor="content">
+            <label className="font-semibold block mb-3" htmlFor="content">
               내용
             </label>
             <FroalaEditor
@@ -111,17 +119,10 @@ function CommunityBoardEditForm(props) {
               }}
             />
           </div>
-
-          <button
-            onClick={handleSubmit}
-            type="button"
-            className="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-            수정
-          </button>
-        </form>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default CommunityBoardEditForm;
+export default CommunityBoardEditForm
--- front/src/pages/boards/MateBoardEditForm.js
+++ front/src/pages/boards/MateBoardEditForm.js
@@ -1,111 +1,134 @@
-import axios from "axios";
-import moment from "moment";
-import React, { useEffect, useState } from "react";
-import Calendar from "react-calendar";
-import FroalaEditor from "react-froala-wysiwyg";
-import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
+import axios from "axios"
+import moment from "moment"
+import React, { useEffect, useState } from "react"
+import Calendar from "react-calendar"
+import FroalaEditor from "react-froala-wysiwyg"
+import { Link, NavLink, useNavigate, useParams } from "react-router-dom"
 import LoadingAnimation from "../../components/LoadingAnimation"
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa"
 
 function MateBoardEditForm(props) {
   //로딩 상태 추가
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
 
-  const { id } = useParams();
+  const { id } = useParams()
 
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState({})
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   // 날짜 관리
-  const [selectedDateRange, setSelectedDateRange] = useState([null, null]);
+  const [selectedDateRange, setSelectedDateRange] = useState([null, null])
 
   // 날짜 초기화
   const handleDateReset = () => {
-    setSelectedDateRange([null, null]); // 날짜 범위를 현재 날짜로 초기화
+    setSelectedDateRange([null, null]) // 날짜 범위를 현재 날짜로 초기화
     setPost({
       ...post,
       startDate: "",
       endDate: "",
-    });
-  };
+    })
+  }
   // 현재 날짜로 돌아오는 함수 추가
   const handleTodayClick = () => {
-    const today = new Date();
+    const today = new Date()
     setPost({
       ...post,
       startDate: today.toLocaleDateString("ko-KR"),
       endDate: today.toLocaleDateString("ko-KR"),
-    });
-    setSelectedDateRange([today, today]); // 현재 날짜로 설정
-  };
+    })
+    setSelectedDateRange([today, today]) // 현재 날짜로 설정
+  }
   // 달력에서 날짜를 선택할 때 호출되는 함수
   const handleDateChange = (dateRange) => {
-    setSelectedDateRange(dateRange);
+    setSelectedDateRange(dateRange)
     setPost({
       ...post,
       startDate: dateRange[0] ? dateRange[0].toLocaleDateString("ko-KR") : "",
       endDate: dateRange[1] ? dateRange[1].toLocaleDateString("ko-KR") : "",
-    });
-  };
+    })
+  }
 
   useEffect(() => {
     // 로딩 애니메이션을 0.5초 동안만 표시
-    setLoading(true);
+    setLoading(true)
     setTimeout(() => {
-      setLoading(false);
-    }, 700);
+      setLoading(false)
+    }, 700)
 
     axios
       .get(`/api/v1/posts/${id}/update`)
       .then((res) => {
-        console.log(res.data);
+        console.log(res.data)
         // 데이터 전달 확인
-        setPost(res.data);
-        setSelectedDateRange([res.data.startDate, res.data.endDate]);
+        setPost(res.data)
+        setSelectedDateRange([res.data.startDate, res.data.endDate])
       })
-      .catch((error) => console.log(error));
-  }, [id]);
+      .catch((error) => console.log(error))
+  }, [id])
 
   //수정 내용 put 요청
   const handleSubmit = () => {
     axios
       .put(`/api/v1/posts/${id}`, post)
       .then((res) => {
-        console.log(post);
+        console.log(post)
         //데이터 전달 확인
-        alert("수정에 성공하였습니다.");
-        navigate(`/posts/mate/${id}/detail`); // 상세 페이지로 돌려보내기
+        alert("수정에 성공하였습니다.")
+        navigate(`/posts/mate/${id}/detail`) // 상세 페이지로 돌려보내기
       })
-      .catch((error) => console.log(error));
-  };
+      .catch((error) => console.log(error))
+  }
 
   // handleChange 처럼 Post 값으로 관리한다.
   const handleModelChange = (e) => {
     setPost({
       ...post,
       content: e,
-    });
-  };
+    })
+  }
 
   const handleChange = (e) => {
     setPost({
       ...post,
       [e.target.name]: e.target.value,
-    });
-  };
+    })
+  }
+
+  // 캘린더의 날짜 스타일을 설정하는 함수 추가
+  const tileClassName = ({ date }) => {
+    const day = date.getDay() // 0: 일요일, 1: 월요일, ..., 6: 토요일
+    // 기본적으로 검은색으로 설정
+    let className = "text-black"
+
+    // 토요일과 일요일에만 빨간색으로 변경
+    if (day === 0 || day === 6) {
+      className = "text-red-500" // 토요일과 일요일에 숫자를 빨간색으로 표시
+    }
+
+    return className // 최종 클래스 이름 반환
+  }
 
   return (
     <div className="container mx-auto p-4 max-w-[900px]">
       {/* 로딩 애니메이션 */}
       {loading && <LoadingAnimation />}
       <div className=" h-full bg-gray-100 p-6">
-        <NavLink
-          className="px-4 py-2 text-sm font-medium rounded-md bg-gray-600 text-gray-100"
-          to={`/posts/mate/${id}/detail`}>
-          상세 페이지로
-        </NavLink>
-
-        <h3 className="mt-3">{id} 번 게시물 수정 폼</h3>
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-3xl font-semibold text-gray-800">여행 코스 수정</h1>
+          <div className="flex space-x-2">
+            <button
+              onClick={() => navigate(`/posts/mate/${id}/detail`)}
+              className="text-gray-700 bg-gray-200 hover:bg-gray-300 rounded-full text-sm px-5 py-2">
+              게시글로 돌아가기
+            </button>
+            <button
+              className="text-white bg-indigo-600 hover:bg-indigo-500 rounded-full text-sm px-5 py-2"
+              onClick={handleSubmit}>
+              수정 완료
+            </button>
+          </div>
+        </div>
 
         {/* 태그 */}
         <div className="flex flex-wrap gap-2 mt-10">
@@ -119,7 +142,7 @@ function MateBoardEditForm(props) {
             ))}
         </div>
 
-        <form>
+        <div>
           <div className="m-3">
             <label className="font-semibold" htmlFor="title">
               제목
@@ -135,36 +158,54 @@ function MateBoardEditForm(props) {
           </div>
 
           <div className="p-4">
+            <label htmlFor="Calendar" className="block font-semibold">
+              일정 기간
+            </label>
+            <div className="my-3">
+              <p>
+                {post.startDate || "0000.00.00."} ~ {post.endDate || "0000.00.00."}
+                <button
+                  onClick={handleDateReset}
+                  className="ml-3 text text-sm bg-tripDuoGreen text-white px-2 py-1 rounded hover:bg-green-600 transition duration-150">
+                  초기화
+                </button>
+              </p>
+            </div>
+
             <Calendar
               selectRange={true}
+              className="w-full p-4 bg-white rounded-lg border-none" // 달력 컴포넌트의 테두리를 없애기 위해 border-none 추가
               onChange={handleDateChange}
               value={selectedDateRange || [new Date(), new Date()]} // 초기값 또는 선택된 날짜 범위
-              formatDay={(locale, date) => moment(date).format("DD")}
               minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
               maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
               navigationLabel={null}
               showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
               calendarType="hebrew" //일요일부터 보이도록 설정
+              tileClassName={tileClassName} // 날짜 스타일 설정
+              formatYear={(locale, date) => moment(date).format("YYYY")} // 네비게이션 눌렀을때 숫자 년도만 보이게
+              formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
+              prevLabel={
+                <FaChevronLeft className="text-green-500 hover:text-green-700 transition duration-150 mx-auto" />
+              }
+              nextLabel={
+                <FaChevronRight className="text-green-500 hover:text-green-700 transition duration-150 mx-auto" />
+              }
+              prev2Label={null}
+              next2Label={null}
+              tileContent={({ date }) => {
+                return (
+                  <span className={date.getDay() === 0 || date.getDay() === 6 ? "text-red-500" : "text-black"}>
+                    {date.getDate()} {/* 날짜 숫자만 표시 */}
+                  </span>
+                )
+              }} // 날짜 내용 설정
+              formatDay={() => null}
             />
-            <button onClick={handleDateReset} className="bg-red-500 text-white px-4 py-2 ml-2">
-              날짜 초기화
-            </button>
-            <button onClick={handleTodayClick} className="bg-green-500 text-white px-4 py-2 ml-2">
-              오늘로 돌아가기
-            </button>
-          </div>
-
-          <div className="my-3">
-            <p>
-              <strong>시작일 : {post.startDate}</strong>
-            </p>
-            <p>
-              <strong>종료일 : {post.endDate}</strong>
-            </p>
           </div>
 
           <div>
-            <label className="font-semibold" htmlFor="content">
+            <label className="font-semibold block mb-3" htmlFor="content">
               내용
             </label>
             <FroalaEditor
@@ -182,10 +223,10 @@ function MateBoardEditForm(props) {
             className="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
             수정
           </button>
-        </form>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default MateBoardEditForm;
+export default MateBoardEditForm
